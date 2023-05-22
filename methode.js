df5f29afa1e80573123051709fa4953c2fd77b83
aicha-808 <125962774+aicha-808@users.noreply.github.com>
2023-05-22 10:24:08 +0000

    Add files via upload

---
+++ methode.js
@@ -0,0 +1,14 @@
+//Récuperer les éléments dans le DOM
+let conteneur = document.getElementById('cont')
+let continput1 = document.getElementById('champ1')
+
+//let continput2 = document.getElementById('champ2')
+let bcalcul = document.getElementById('calculer')
+let somme = document.getElementById('resultat')
+let difference = document.getElementById('resultat1')
+let produit = document.getElementById('resultat2')
+let division = document.getElementById('resultat3')
+
+ //Récuperer la valeur saisie par l'utulisateur
+let x =  parseFloat(document.getElementById('val1').value)
+let  y =  parseFloat(document.getElementById('val2').value)
